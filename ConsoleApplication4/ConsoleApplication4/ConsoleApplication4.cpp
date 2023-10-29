356c2b204f06a4482804db2c12d1ff3052e44637
uninaniso <128987305+uninaniso@users.noreply.github.com>
2023-10-29 15:26:22 +0200

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+class Subsequence {
+    int first;
+    int second;
+    int result;
+
+public:
+    void search(int number) {
+        if (this->first == number) {
+            this->result = number;
+        }
+
+        else if (this->second == number) {
+            this->result = number;
+        }
+
+        else cout << "\nerror" << endl;
+    }
+
+    Subsequence() {
+        this->first = rand() % 100;
+        this->second = rand() % 100;
+
+        cout << "\nfirst " << this->first;
+        cout << "\nsecond " << this->second;
+
+        int number;
+        cout << "\nnumber:";
+        cin >> number;
+        search(number);
+        if (this->result != this->first && this->result != this->second)this->result = 0;
+        cout << "result " << this->result;
+    }
+};
+
+int main()
+{
+    Subsequence s;
+}
