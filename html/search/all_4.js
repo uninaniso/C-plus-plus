1b4a84311f6b7705097852202b0b61f9339a3976
uninaniso <128987305+uninaniso@users.noreply.github.com>
2024-01-15 20:05:31 +0200

    Add files via upload

---
+++ html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['math_0',['Math',['../class_calculator.html#ad81c9b5dbde49ec678fa528e68a077cd',1,'Calculator']]]
+];
---
+++ html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['calculator_0',['Calculator',['../class_calculator.html',1,'']]]
+];
